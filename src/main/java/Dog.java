75227af156ec99e9a5372656004781674f47cf90
H-Yous <onlinehumza@gmail.com>
2019-04-15 15:08:31 +0100

    Init Commit

---
+++ src/main/java/Dog.java
@@ -0,0 +1,14 @@
+
+public class Dog {
+	public String name;
+	
+	public Dog() {
+		// TODO Auto-generated constructor stub
+	name = "Dog";
+	
+	}
+	
+	public String bark() {
+		return "Woof";
+	}
+}
---
+++ src/main/java/runThis.java
@@ -0,0 +1,13 @@
+class Dog.java;
+
+
+public class runThis {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World");
+		Dog humza = new Dog();
+		
+	}
+
+}
